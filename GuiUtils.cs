2d87a40190d1f4a3011139125920f6724a34c2d4
Adrian Skilling <adrian.skilling@gmail.com>
2020-07-13 23:27:39 +0100

    Add pick a target and refactored Enable()

---
+++ GuiUtils.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using UnityEngine;
+
+namespace HopperGuidance
+{
+    static public class GuiUtils
+    {
+        public static bool GetMouseHit(CelestialBody body, out RaycastHit hit)
+        {
+          // Cast a ray from screen point
+          Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+          bool isHit = Physics.Raycast(ray.origin, ray.direction, out hit, Mathf.Infinity);
+          return isHit;
+        }
+    }
+}
