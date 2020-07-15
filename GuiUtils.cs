c12aaf2a4b54daaf4c17ec2ea8de0eb40bb4a6a2
Adrian Skilling <adrian.skilling@gmail.com>
2020-07-15 22:42:34 +0100

    Stopped click through on part window

--- GuiUtils.cs
+++ GuiUtils.cs
@@ -6,8 +6,21 @@ namespace HopperGuidance
 {
     static public class GuiUtils
     {
-        public static bool GetMouseHit(CelestialBody body, out RaycastHit hit)
+        public static bool OverPartWindow(Part part, Vector2d mouse)
         {
+          bool symmetry = false;
+          UIPartActionWindow window = UIPartActionController.Instance.GetItem(part, symmetry);
+          return window.Hover;
+        }
+
+        public static bool GetMouseHit(CelestialBody body, out RaycastHit hit, Part part)
+        {
+          if ((part!=null) && OverPartWindow(part, Input.mousePosition))
+          {
+            hit = new RaycastHit();
+            return false;
+          }
+
           // Cast a ray from screen point
           Ray ray = FlightCamera.fetch.mainCamera.ScreenPointToRay(Input.mousePosition);
           bool isHit = Physics.Raycast(ray.origin, ray.direction, out hit, Mathf.Infinity, 1 << 15);
