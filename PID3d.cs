32242e6847640f7530635802e69a286e0efa33a6
Adrian Skilling <adrian.skilling@gmail.com>
2020-07-26 12:55:12 +0100

    Changed parameters to make it work better with a wider range of craft

--- PID3d.cs
+++ PID3d.cs
@@ -4,8 +4,9 @@ namespace HopperGuidance
   {
     PIDclamp pid1x,pid1y,pid1z;
     PIDclamp pid2x,pid2y,pid2z;
+    float _vMult;
 
-    public void Init(float kp1, float ki1, float kd1, float kp2, float ki2, float kd2, float vmax, float amax, float ymult=1.0f)
+    public void Init(float kp1, float ki1, float kd1, float kp2, float ki2, float kd2, float vmax, float amax, float ymult=1.0f, float vMult=1)
     {
       // Probably set I1=0, D1=0, I2=0, D2=0
       // P1 determines proportion of position error to velocity 1, to close 1m as 1m/s
@@ -13,12 +14,14 @@ namespace HopperGuidance
       // I1>0 would help overcome something like a constant wind or gravity (but can compensate exactly for gravity)
       // ymult scales up P1 for make the PID focus on hitting the height target
       // (this is easier since we have gravity to pull us down if overshooting upward)
+      _vMult = vMult;
       pid1x = new PIDclamp("pid1x",kp1,ki1,kd1,vmax);
       pid1y = new PIDclamp("pid1y",kp1*ymult,ki1,kd1,vmax);
-      pid1z = new PIDclamp("pid1y",kp1,ki1,kd1,vmax);
+      pid1z = new PIDclamp("pid1z",kp1,ki1,kd1,vmax);
+      // Scale up amax so its effectively ignored
       pid2x = new PIDclamp("pid2x",kp2,ki2,kd2,amax);
       pid2y = new PIDclamp("pid2y",kp2*ymult,ki2,kd2,amax);
-      pid2z = new PIDclamp("pid2y",kp2,ki2,kd2,amax);
+      pid2z = new PIDclamp("pid2z",kp2,ki2,kd2,amax);
     }
 
     public void Reset()
@@ -37,15 +40,16 @@ namespace HopperGuidance
       Vector3d err_v = dv - v;
 
       // Compute modified velocity targets given position and velocity error
-      tgt_v1 = new Vector3d(pid1x.Update(err_r.x + err_v.x, dt),
-                            pid1y.Update(err_r.y + err_v.y, dt),
-                            pid1z.Update(err_r.z + err_v.z, dt));
+      tgt_v1 = new Vector3d(pid1x.Update(err_r.x + err_v.x*_vMult, dt),
+                            pid1y.Update(err_r.y + err_v.y*_vMult, dt),
+                            pid1z.Update(err_r.z + err_v.z*_vMult, dt));
 
 
       // Execute PID 2's to give acceleration
       Vector3d a = new Vector3d(pid2x.Update(tgt_v1.x, dt),
                                 pid2y.Update(tgt_v1.y, dt),
                                 pid2z.Update(tgt_v1.z, dt));
+      //Debug.Log("err_r="+(Vector3)err_r+" err_v="+(Vector3)err_v+" tgt_v="+(Vector3d)tgt_v1+" a="+(Vector3)a);
 
       return a;
     }
