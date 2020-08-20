6971119cf55f7022e283d64a0d6704c193622904
Adrian Skilling <adrian.skilling@gmail.com>
2020-08-20 06:54:02 +0100

    Can show times and minDescentAngle per datafilie

--- plotXYZ.py
+++ plotXYZ.py
@@ -12,30 +12,14 @@ def plot_line(ax,data,fx,fy,color='black',label=''):
   ax.plot(xx,yy,color=color,label=label)
 
 
-def plot_times(ax, times):
+def plot_times(ax, times, color):
   for t in times:
-    ax.axvspan(t-0.1,t+0.1,facecolor='g',alpha=0.5)
+    ax.axvspan(t-0.1,t+0.1,facecolor=color,alpha=0.5)
 
-def plot_checks(ax,data,fx,fy,checkGapFirst,checkGapMult,color='red'):
-  if not checkGapFirst or not checkGapMult:
-    return
-  T = data[-1]['time']
-  cx,cy=[],[]
-  tX=[]
-  gap = checkGapFirst
-  t = checkGapFirst
-  while(t < 0.5*T):
-    tX.append(t)
-    t = t + gap
-    gap = gap * checkGapMult
-  gap = checkGapFirst
-  t = checkGapFirst
-  while(t < 0.5*T):
-    tX.append(T-t)
-    t = t + gap
-    gap = gap * checkGapMult
-
-  for t in tX:
+def plot_checks(ax,data,fx,fy,times,color='black'):
+  cx = []
+  cy = []
+  for t in times:
     done = False
     for i,d in enumerate(data):
       if data[i]['time'] >= t and (not done):
@@ -45,127 +29,152 @@ def plot_checks(ax,data,fx,fy,checkGapFirst,checkGapMult,color='red'):
   ax.plot(cx,cy,color=color,marker='o',markersize=10,linestyle='')
 
 
-def plot(datas,labels,xmin,xmax,ymin,ymax,zmin,zmax,
-         vxmin,vxmax,vymin,vymax,vzmin,vzmax,tmax=30,amult=1,askip=3,checkGapFirst=None,checkGapMult=None,minDescentAngle=None,
-         amin=None,amax=None,
-         filenames=[], thrust_times=[]):
+def plot(labels,xmin,xmax,ymin,ymax,zmin,zmax,
+         vxmin,vxmax,vymin,vymax,vzmin,vzmax,tmax=30,amult=1,askip=3,
+         filenames=[],showchecks=False):
+
+  # Set up figures
+  # altitude against time, and throttle
+  fig = P.figure(1)
 
   colors=['red','blue','green','black','pink','grey','purple','salmon']
   alim = [0,40]
 
-  # altitude against time, and throttle
-  fig = P.figure(1)
-
   P.subplot2grid((3,5),(0,0), colspan=1, rowspan=1)
-  ax = P.gca()
-  ax.set_xlabel("time")
-  ax.set_ylabel("x")
-  ax.set_xlim([0,tmax])
-  ax.set_ylim([xmin,xmax])
-  for col,data in zip(colors,datas):
-    plot_line(ax,data,'time','x',color=col)
-    plot_checks(ax,data,'time','x',checkGapFirst,checkGapMult)
-  ax.grid()
+  ax1 = P.gca()
+  ax1.set_xlabel("time")
+  ax1.set_ylabel("x")
+  ax1.set_xlim([0,tmax])
+  ax1.set_ylim([xmin,xmax])
+  ax1.grid()
 
   P.subplot2grid((3,5),(0,1), colspan=1, rowspan=1)
-  ax = P.gca()
-  ax.set_xlabel("time")
-  ax.set_ylabel("vx")
-  ax.set_xlim([0,tmax])
-  ax.set_ylim([vxmin,vxmax])
-  for col,data in zip(colors,datas):
-    plot_line(ax,data,'time','vx',color=col)
-    plot_checks(ax,data,'time','vx',checkGapFirst,checkGapMult)
-  ax.grid()
+  ax2 = P.gca()
+  ax2.set_xlabel("time")
+  ax2.set_ylabel("vx")
+  ax2.set_xlim([0,tmax])
+  ax2.set_ylim([vxmin,vxmax])
+  ax2.grid()
 
   P.subplot2grid((3,5),(1,0), colspan=1, rowspan=1)
-  ax = P.gca()
-  ax.set_xlabel("time")
-  ax.set_ylabel("y")
-  ax.set_xlim([0,tmax])
-  ax.set_ylim([ymin,ymax])
-  for col,data in zip(colors,datas):
-    plot_line(ax,data,'time','y',color=col)
-    plot_checks(ax,data,'time','y',checkGapFirst,checkGapMult)
-  ax.grid()
+  ax3 = P.gca()
+  ax3.set_xlabel("time")
+  ax3.set_ylabel("y")
+  ax3.set_xlim([0,tmax])
+  ax3.set_ylim([ymin,ymax])
+  ax3.grid()
 
   P.subplot2grid((3,5),(1,1), colspan=1, rowspan=1)
-  ax = P.gca()
-  ax.set_xlabel("time")
-  ax.set_ylabel("vy")
-  ax.set_xlim([0,tmax])
-  ax.set_ylim([vymin,vymax])
-  for col,data in zip(colors,datas):
-    plot_line(ax,data,'time','vy',color=col)
-    plot_checks(ax,data,'time','vy',checkGapFirst,checkGapMult)
-  ax.grid()
+  ax4 = P.gca()
+  ax4.set_xlabel("time")
+  ax4.set_ylabel("vy")
+  ax4.set_xlim([0,tmax])
+  ax4.set_ylim([vymin,vymax])
+  ax4.grid()
 
   P.subplot2grid((3,5),(2,0), colspan=1, rowspan=1)
-  ax = P.gca()
-  ax.set_xlabel("time")
-  ax.set_ylabel("z")
-  ax.set_xlim([0,tmax])
-  ax.set_ylim([zmin,zmax])
-  for col,data in zip(colors,datas):
-    plot_line(ax,data,'time','z',color=col)
-    plot_checks(ax,data,'time','z',checkGapFirst,checkGapMult)
-  ax.grid()
-  ax.legend()
+  ax5 = P.gca()
+  ax5.set_xlabel("time")
+  ax5.set_ylabel("z")
+  ax5.set_xlim([0,tmax])
+  ax5.set_ylim([zmin,zmax])
+  ax5.grid()
+  ax5.legend()
+
   P.subplot2grid((3,5),(2,1), colspan=1, rowspan=1)
-  ax = P.gca()
-  ax.set_xlabel("time")
-  ax.set_ylabel("vz")
-  ax.set_xlim([0,tmax])
-  ax.set_ylim([vzmin,vzmax])
-  for col,data in zip(colors,datas):
-    plot_line(ax,data,'time','vz',color=col)
-    plot_checks(ax,data,'time','vz',checkGapFirst,checkGapMult)
-  ax.grid()
+  ax6 = P.gca()
+  ax6.set_xlabel("time")
+  ax6.set_ylabel("vz")
+  ax6.set_xlim([0,tmax])
+  ax6.set_ylim([vzmin,vzmax])
+  ax6.grid()
 
   # Throttle
   P.subplot2grid((3,5),(0,2), colspan=2, rowspan=1)
-  ax = P.gca()
-  ax.set_xlabel("time")
-  ax.set_ylabel("mag(accel)")
-  ax.set_xlim([0,tmax])
-  ax.set_ylim(alim)
-
-  # plot desired magnitude of acceleration
-  tdata = []
-  for col,data in zip(colors,datas):
-    for d in data:
-      T=np.array([d['ax'],d['ay'],d['az']])
-      d['mag_accel'] = np.linalg.norm(T)
-    plot_line(ax,data,'time','mag_accel',color=col)
-    plot_checks(ax,data,'time','mag_accel',checkGapFirst,checkGapMult,color=col)
-    if amin:
-      ax.plot([0,data[-1]['time']],[amin,amin],color='blue',linestyle='--')
-    if amax:
-      ax.plot([0,data[-1]['time']],[amax,amax],color='blue',linestyle='--')
-  plot_times(ax, thrust_times)
-  ax.grid()
+  ax7 = P.gca()
+  ax7.set_xlabel("time")
+  ax7.set_ylabel("mag(accel)")
+  ax7.set_xlim([0,tmax])
+  ax7.set_ylim(alim)
+  ax7.grid()
 
   # Attitude error
   P.subplot2grid((3,5),(0,4), colspan=1, rowspan=1)
-  ax = P.gca()
-  ax.set_xlabel("time")
-  ax.set_ylabel("attitude error /degrees")
-  ax.set_xlim([0,tmax])
-  ax.set_ylim([0,90])
-  for col,data in zip(colors,datas):
-    plot_line(ax,data,'time','att_err',color=col)
-  ax.grid()
+  ax8 = P.gca()
+  ax8.set_xlabel("time")
+  ax8.set_ylabel("attitude error /degrees")
+  ax8.set_xlim([0,tmax])
+  ax8.set_ylim([0,90])
+  ax8.grid()
 
   # XY
   P.subplot2grid((3,5),(1,2), colspan=3, rowspan=2)
-  ax = P.gca()
-  ax.set_xlabel("x")
-  ax.set_ylabel("y")
-  ax.set_xlim([xmin,xmax])
-  ax.set_ylim([ymin,ymax])
+  ax9 = P.gca()
+  ax9.set_xlabel("x")
+  ax9.set_ylabel("y")
+  ax9.set_xlim([xmin,xmax])
+  ax9.set_ylim([ymin,ymax])
+  ax9.legend()
+  ax9.grid()
 
-  # plot side view of X,Y
-  for di,data in enumerate(datas):
+
+  for di,filename in enumerate(filenames):
+    col = colors[di]
+    data = read_data(filename,info)
+    thrust_times = []
+    check_times = []
+    if 'minDescentAngle' in info:
+      minDescentAngle = float(info['minDescentAngle'])
+    else:
+      minDescentAngle = None
+    amin = 0
+    if 'amin' in info:
+      amin = float(info['amin'])
+    amax = 30
+    if 'amax' in info:
+      amax = float(info['amax'])
+    if 'thrust_times' in info:
+      thrust_times = [float(t) for t in info['thrust_times'].split(",")]
+    if 'check_times' in info:
+      check_times = [float(t) for t in info['check_times'].split(",")]
+    if not showchecks:
+      check_times = []
+
+    plot_line(ax1,data,'time','x',color=col)
+    plot_checks(ax1,data,'time','x',check_times,color=col)
+
+    plot_line(ax2,data,'time','vx',color=col)
+    plot_checks(ax2,data,'time','vx',check_times,color=col)
+
+    plot_line(ax3,data,'time','y',color=col)
+    plot_checks(ax3,data,'time','y',check_times,color=col)
+
+    plot_line(ax4,data,'time','vy',color=col)
+    plot_checks(ax4,data,'time','vy',check_times,color=col)
+
+    plot_line(ax5,data,'time','z',color=col)
+    plot_checks(ax5,data,'time','z',check_times,color=col)
+
+    plot_line(ax6,data,'time','vz',color=col)
+    plot_checks(ax6,data,'time','vz',check_times,color=col)
+
+    # plot desired magnitude of acceleration
+    tdata = []
+    for d in data:
+      T=np.array([d['ax'],d['ay'],d['az']])
+      d['mag_accel'] = np.linalg.norm(T)
+    plot_line(ax7,data,'time','mag_accel',color=col)
+    plot_checks(ax7,data,'time','mag_accel',check_times,color=col)
+    if amin:
+      ax7.plot([0,data[-1]['time']],[amin,amin],color='blue',linestyle='--')
+    if amax:
+      ax7.plot([0,data[-1]['time']],[amax,amax],color='blue',linestyle='--')
+    plot_times(ax7, thrust_times, color=col)
+
+    plot_line(ax8,data,'time','att_err',color=col)
+
+
+    # plot side view of X,Y
     xx,yy=[],[]
     for i,d in enumerate(data):
       xx = []
@@ -174,26 +183,24 @@ def plot(datas,labels,xmin,xmax,ymin,ymax,zmin,zmax,
       yy.append(d['y'])
       xx.append(d['x']+d['ax']*amult)
       yy.append(d['y']+d['ay']*amult)
-      ax.plot(xx,yy,color=colors[di],alpha=0.5)
-    plot_line(ax,data,'x','y',color=colors[di],label=filenames[di])
+      ax9.plot(xx,yy,color=colors[di],alpha=0.5)
+    plot_line(ax9,data,'x','y',color=colors[di],label=filenames[di])
     # Show checkpoints
-    plot_checks(ax,data,'x','y',color=colors[di],checkGapFirst=checkGapFirst,checkGapMult=checkGapMult)
+    plot_checks(ax9,data,'x','y',check_times,color=colors[di])
 
   # Draw min descent angle
-  if minDescentAngle:
-    fx = datas[0][-1]['x']
-    fy = datas[0][-1]['y']
-    fx = 0
-    dy = 0
-    s = sin(radians(minDescentAngle))
-    c = cos(radians(minDescentAngle))
-    d = (xmax-xmin) + (ymax-ymin)
-    xx = [-d*c + fx,fx,d*c + fx]
-    yy = [d*s + fy,fy,d*s + fy]
-    ax.plot(xx,yy,color='blue',linestyle='--')
+    if minDescentAngle is not None:
+      fx = datas[0][-1]['x']
+      fy = datas[0][-1]['y']
+      fx = 0
+      dy = 0
+      s = sin(radians(minDescentAngle))
+      c = cos(radians(minDescentAngle))
+      d = (xmax-xmin) + (ymax-ymin)
+      xx = [-d*c + fx,fx,d*c + fx]
+      yy = [d*s + fy,fy,d*s + fy]
+      ax9.plot(xx,yy,color=colors[di],linestyle='--')
 
-  ax.legend()
-  ax.grid()
   P.show()
 
 def extract_items(line):
@@ -213,7 +220,6 @@ def read_data(fname, d):
     line=line.strip("\n\r")
     if line.startswith("#"):
       dd = extract_items(line[1:])
-      print(dd)
       d.update(dd)
       continue
     if not fields:
@@ -243,13 +249,9 @@ parser.add_argument('--vymax', type=float, help='Maximum vy position', default=N
 parser.add_argument('--vzmin', type=float, help='Minimum vz position', default=None)
 parser.add_argument('--vzmax', type=float, help='Maximum vz position', default=None)
 parser.add_argument('--tmax', type=float, help='Maximum time', default=None)
-parser.add_argument('--amin', type=float, help='Show minimum acceleration constraint', default=None)
-parser.add_argument('--amax', type=float, help='Show maximum acceleration constraint', default=None)
-parser.add_argument('--checkGapFirst', type=float, help='Time of first check from start and end', default=None)
-parser.add_argument('--checkGapMult', type=float, help='Multiplier to set time to next check (use >1)', default=None)
-parser.add_argument('--minDescentAngle', type=float, help='Show cone for minimum descent angle', default=None)
 parser.add_argument('--amult', type=float, help='Multiplier for scale up thrust acceleration lines', default=1)
 parser.add_argument('--square', action='store_true', help='Make XY plot square (roughly as depends on window size)', default=False)
+parser.add_argument('--showchecks', action='store_true', help='Show time checks for max vel. and min descent angle', default=False)
 
 args = parser.parse_args()
 
@@ -312,6 +314,6 @@ if args.square:
     args.ymin = args.ymin - (width-height)*0.5
     args.ymax = args.ymax + (width-height)*0.5
 
-plot(datas,args.filename,xmin=args.xmin,xmax=args.xmax,ymin=args.ymin,ymax=args.ymax,zmin=args.zmin,zmax=args.zmax,
+plot(args.filename,xmin=args.xmin,xmax=args.xmax,ymin=args.ymin,ymax=args.ymax,zmin=args.zmin,zmax=args.zmax,
      vxmin=args.vxmin,vxmax=args.vxmax,vymin=args.vymin,vymax=args.vymax,vzmin=args.vzmin,vzmax=args.vzmax,tmax=args.tmax,
-     amult=args.amult,checkGapFirst=args.checkGapFirst,checkGapMult=args.checkGapMult,filenames=args.filename,minDescentAngle=args.minDescentAngle,amin=args.amin,amax=args.amax,thrust_times=thrust_times)
+     amult=args.amult,filenames=args.filename,showchecks=args.showchecks)
